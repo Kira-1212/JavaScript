333477a3bf7515c24cddbc810d59ff776606be9f
Sai Kiran Davuluri <saikiran.davuluri@gmail.com>
2022-03-31 23:29:45 -0700

    Added the codes developed for UI course

---
+++ UI/data2.js
@@ -0,0 +1,22 @@
+const people = [
+    {
+      img:
+        "./story2/1.jpg"
+    },
+    {
+      img:
+      "./story2/2.jpg"
+      
+    },
+    {
+        img:
+        "./story2/3.jpg"
+        
+      },
+    {
+      img:
+      "./story2/4.jpg"
+    }
+  ];
+  
+  export default people;
---
+++ UI/data3.js
@@ -0,0 +1,8 @@
+const people = [
+    {
+      img:
+        "./story3/1.jpg"
+    }
+  ];
+  
+  export default people;
